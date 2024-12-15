ac0f799e3b838bb4b6c0ef5d56b5ac5f483b9304
Marlen <managadzem5@gmail.com>
2024-12-15 16:38:29 +0400

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,42 @@
 "use strict"
+
 function solveEquation(a, b, c) {
+
   let arr = [];
-  
-  return arr;
+  let d = b**2 - 4 * a * c;
+
+  if (d < 0) {
+
+    return arr;
+
+  } else if ( d === 0) {
+
+    let x = -b / (2 * a);
+
+    arr.push(x);
+    return arr;
+
+  } else {
+
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+    arr.push(x1, x2);
+    return arr;
+
+  }
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = (percent / 100) / 12;
+  let S = amount - contribution;
+  let n = countMonths;
+
+  let payment = S * (P + (P / (((1 + P)**n) - 1)))
+  let totalPayment = payment * n;
+  let totalAmount = +totalPayment.toFixed(2);
+
+  return totalAmount;
 }
+
