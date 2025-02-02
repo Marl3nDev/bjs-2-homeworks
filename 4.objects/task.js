8eab05643cdb9b81d8360af7c08d81fd4fb77a5d
Marlen <managadzem5@gmail.com>
2025-02-02 23:11:42 +0400

    сделал objects и classes

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,47 @@
 function Student(name, gender, age) {
-  
+  this.name = name,
+  this.gender = gender,
+  this.age = age,
+  this.marks = [],
+  this.status = "active"
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+let StudentOne = new Student("Ivan", "male", 21)
+let StudentTwo = new Student("Maria", "female", 19)
+
+
+Student.prototype.setSubject = function (subject) {
+  this.subject = subject;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.status === "active") {
+    this.marks.push(...marks)
+  } else {
+    console.log(`Студент отчислен!`)
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+
+  if (!this.marks || this.marks.length === 0) { 
+    return 0; 
+  } else {
+    let sum = 0;
+    for (let i = 0; i<this.marks.length; i++) {
+    sum += this.marks[i];};
+    return sum/this.marks.length
+};
 }
+ 
 
 Student.prototype.exclude = function (reason) {
-  
+  if (this.status === "active"){
+    this.status = "expelled"
+    delete this.subject;
+    delete this.marks;
+
+    this.excluded = reason;
+  }
 }
+
